1da7402b97cf56d3ffc169fa37df193a71805053
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-21 12:47:57 -0300

    Finishing example code

--- server.js
+++ server.js
@@ -21,11 +21,20 @@ server.listen(5000, function () {
 	console.log('Starting server on port 5000');
 });
 
+
+var players = {};
 // Add the WebSocket handlers
 io.on('connection', function(socket) {
-
+	socket.on('new player', function () {
+		players[socket.id] = { x: 300, y: 300 }
+	});
+	socket.on('movement', function (data) {
+		var player = players[socket.id] || {};
+		if (data.left) player.x -= 5;
+		if (data.up) player.y -= 5;
+		if (data.right) player.x += 5;
+		if (data.down) player.y += 5;
+	})
 });
 
-setInterval(function() {
-  	io.sockets.emit('message', 'hi!');
-}, 1000);
+setInterval(() => io.sockets.emit('state', players), 1000 / 60);
